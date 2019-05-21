ee7d6a33566fd00899bfd8e8fb93ab7565ae930d
Gareth Howell <gareth.howell@gmail.com>
2019-05-21 20:02:19 +0100

    Copied from piDashcam

---
+++ src/pi-telemetry/__init__.py
@@ -0,0 +1,1 @@
+from .pi-telemetry import PiTelemetry
---
+++ src/pi-telemetry/pi-telemetry.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python -p
+
+"""pi-telemetry Raspberry Pi telemetry module
+
+A systemd compliant daemon to read data from i2c sensors connected
+to a Raspberry Pi and send via MQTT
+
+"""
+# Standard libraries
+import io, os, sys
+from time import time, sleep
+import logging
+
+# Specials
+import RPi.GPIO as GPIO
+
+class PiTelemetry():
+    """
+    PiTelemetry -
+    """
+    def __init__(self, mqtt_broker, mqtt_base_topic):
+        self.log = logging.getlogger(__name__)
+        self.log.debug("PiTelemetry.__init__()")
+
+        self.broker = mqtt_broker
+        self.base_topic = mqtt_base_topic
+
+    def run(self):
+        self.log.debug("PiTelemetry.run()")
+
+        #Setup
+
+        #Main Loop
+
+        #Shutdown
+        self.log.debug("Shutting down")
