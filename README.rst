4d1dda156dd26bc51561c7e38081e76042beaddb
Gareth Howell <gareth.howell@gmail.com>
2020-06-09 10:51:06 +0100

    Change of syntax stubs for registering sensor with HA

--- README.rst
+++ README.rst
@@ -3,38 +3,34 @@ pitelemetry
 
 Collect data from 1-wire sensors connected to a Raspberry Pi and send via mqtt
 
-another line
+It takes two (optional) ArgumentParser
+
+-  `-c, --configfile` the yaml file that contains the configuration. The default configuration file is `/etc/pitelemetry.yaml`
+
+- `-l --loglevel` the logging level (INFO, WARNING, ERROR, DEBUG). The default is INFO
 
 
-It takes one argument - the yaml file that contains the configuration.
-The default configuration file is
-
-this is text
-
-
-so is this
-
-
-
-An example configuration is:
+An example configuration is::
 
  broker:
     mqtt_client: mynode
     mqtt_broker: mqtt.foo.bar
     mqtt_port: 1833
     update_interval: 60 # seconds
+
+  discovery_prefix: "home-assistant"
+
   sensors:
     - sensor:
       name: baz_temp
       type: temperature
       device: 28-0417c15da2ae
-      topic: tel/foo/baz/temp
+      state_topic: tel/foo/baz/temp
     - sensor:
       name: external_temp
       type: temperature
       device: 28-0417c15cf333
-      topic: tel/foo/ext/temp
-
+      state_topic: tel/foo/ext/temp
 
 RESTRICTIONS
 ------------
@@ -45,11 +41,11 @@ but is easily extended to support other types.
 INSTALLATION
 ------------
 
-Execute the following from a terminal
+Execute the following from a terminal::
 
  sudo pip3 install pitelemetry
- sudo systemctl enable /etc/systemd/system/pitelemetry
- sudo systemctl start pitelemetry.service
+  sudo systemctl enable /etc/systemd/system/pitelemetry
+  sudo systemctl start pitelemetry.service
 
 
 pitelemetry currently supports the following sensors:
@@ -62,7 +58,7 @@ ADDING NEW SENSOR TYPES
 To add a new sensor type:
 
 * Clone the `PiTemperature` class
-* Modify the `_read_device()` function as needed
+* Modify the `_read_sensor()` function as needed
 * Add the new sensor to the `sensorTypes` list in `bin/pitelemetry`
 
 TODO
--- src/pitelemetry/pitelemetry.py
+++ src/pitelemetry/pitelemetry.py
@@ -25,7 +25,7 @@ class PiTelemetry(Thread):
     source = None
     log = None
 
-    def __init__(self, broker, sensor, shutdown):
+    def __init__(self, broker, discovery_prefix, sensor, shutdown):
         ''' Constructor. '''
         Thread.__init__(self)
         self.log=logging.getLogger(__name__)
@@ -33,14 +33,19 @@ class PiTelemetry(Thread):
 
         # unpack the config
         self.broker = broker
+        self.discovery_prefix = discovery_prefix
         self.sensor = sensor
         self.shutdown = shutdown
 
 
     # Private functions
 
-    def _read_device(self,device):
-        raise NotImplementedError("_read_device is implemented in the sub-classes")
+    def _register_device(self, sensor):
+        """Register the sensor with Home Assistant"""
+        self.log.debug("Registering sensor with Home Assistant. sensor: %s", sensor)
+
+    def _read_sensor(self, sensor):
+        raise NotImplementedError("_read_sensor is implemented in the sub-classes")
 
     def connected(client, userdata, flags, rc):
         """Callback function for when the client receives a CONNACK response from the broker """
@@ -57,28 +62,28 @@ class PiTelemetry(Thread):
         self.log.debug("Message received from broker. topic: %s, message: %s", msg.topic, str(msg.payload))
 
     def run(self):
-        """Connect to the broker and start reporting"""
+        """Connect to the broker, register the sensor and start reporting"""
         self.log.debug("pitelemetry run")
         self.log.debug("%s is running", __name__)
 
         # Setup parameters for connection to broker
-        mqttClient = self.broker['mqtt_client']+'_'+self.name
-        mqttBroker = self.broker['mqtt_broker']
-        mqttUser = self.broker['mqtt_user']
-        mqttPassword = self.broker['mqtt_password']
+        mqtt_client = self.broker['mqtt_client']+'_'+self.name
+        mqtt_broker = self.broker['mqtt_broker']
+        mqtt_user = self.broker['mqtt_user']
+        mqtt_password = self.broker['mqtt_password']
 
         # Setup topic and sensor
-        mqttTopic = self.sensor['topic']
-        self.log.debug("mqttClient = %s, mqttBroker=%s, mqttTopic=%s",mqttClient, mqttBroker,mqttTopic)
-        w1Device = self.sensor['device']
+        mqtt_topic = self.sensor['state_topic']
+        self.log.debug("mqtt_client = %s, mqtt_broker=%s, mqtt_topic=%s",mqtt_client, mqtt_broker,mqtt_topic)
+        w1_device = self.sensor['device']
 
         # Make sure we access the right device
-        baseDir = '/sys/bus/w1/devices/'
-        device = baseDir + w1Device + '/w1_slave'
+        base_dir = '/sys/bus/w1/devices/'
+        sensor = base_dir + w1_device + '/w1_slave'
 
         # Create an MQTT client instance
         # client = MQTTClient(mqttUser, mqttPassword, mqttBroker, False) # Use insecure connection to this internal broker
-        client = MQTTClient(mqttUser, mqttPassword, mqttBroker, False) # Use insecure connection to this internal broker
+        client = MQTTClient(mqtt_user, mqtt_password, mqtt_broker, False) # Use insecure connection to this internal broker
 
         # Setup the callbacks
         client.on_connect = self.connected
@@ -92,11 +97,15 @@ class PiTelemetry(Thread):
             #Set the client to loop in the background
             client.loop_background()
 
+            # Register the sensor with Home Assistant (if used)
+            if not self.discovery_prefix == "":
+                self.log.debug("registering sensor with Home Assistant. sensor: %s, registration topic: %s, value: %s", sensor, topic, sensor_string)
+
             # Start reading device data and publishing to broker
             while True:
-                data = self._read_device(device)
-                client.publish(mqttTopic, data) # Publish
-                self.log.debug("Published %s to %s", data, mqttTopic)
+                data = self._read_sensor(sensor)
+                client.publish(mqtt_topic, data) # Publish
+                self.log.debug("Published %s to %s", data, mqtt_topic)
                 time.sleep(self.broker['update_interval'])
 
         #Shutdown
